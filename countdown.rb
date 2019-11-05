d74b6bbf9192a7a6329301e271fe242e12be0cab
kasperken <kennethandrews@protonmail.com>
2019-11-05 09:53:52 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
-
-def countdown
+def countdown(countdown)
+  until countdown <= 0 do
+    puts("#{countdown} SECOND(S)!")
+    countdown -= 1
+  end
+  ("HAPPY NEW YEAR!")
 end
+
+def countdown_with_sleep(countdown)
+  until countdown <= 0 do
+    puts ("#{countdown} SECOND(S)!")
+    sleep(1)
+    countdown -= 1
+  end
+  ("HAPPY NEW YEAR!")
+end
+
+countdown(10)
+countdown_with_sleep(10)
+
