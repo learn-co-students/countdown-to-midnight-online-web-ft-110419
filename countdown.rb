c21b281c702ca7faaf5004b350f59f1ab2f5cbba
Ken Goad <goadke@g.cofc.edu>
2019-11-06 21:55:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(num_num)
+  while num_num > 0 do
+  puts "#{num_num} SECOND(S)!"
+  num_num -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num_num)
+  while num_num > 0 do
+    puts "#{num_num} SECOND(S)!"
+    num_num -= 1
+    sleep 1
+  end
+  "HAVING FLU BEER!"
 end
